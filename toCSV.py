80cf26d2ddeb8c3daeeefb98974564ad272c4c55
Mahirah Samah <mahirah.samah@tamu.edu>
2022-04-12 18:35:29 -0500

    db connection done

--- toCSV.py
+++ toCSV.py
@@ -1,4 +1,21 @@
 # imports
+
+import csv
+
+def dict_to_csv(dictionary):
+
+    
+    filename = "dictionary_file.csv"
+    with open (filename, "w", newline="") as csvfile:
+        fields = ["keyword", "definition"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fields)
+        writer.writeheader()
+        #writer.writerow(dictionary[1])
+        for key in dictionary:
+            writer.writerow({fields[0]: key, fields[1]: dictionary[key]})
+        #filename.close()
+    #return 1
+'''
 from flask import Flask, render_template, request, redirect, jsonify, make_response
 import csv
 import os, io
@@ -18,3 +35,5 @@ def dict_to_csv(dictionary):
             writer.writerow({fields[0]: key, fields[1]: dictionary[key]})
         #filename.close()
     #return 1
+
+'''
