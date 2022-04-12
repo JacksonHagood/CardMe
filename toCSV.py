dea0dd535bd0845cb9a0ce151104b32b72415b45
Jackson Hagood <hagoojac@tamu.edu>
2022-04-12 09:29:07 -0500

    Add files via upload

--- toCSV.py
+++ toCSV.py
@@ -1,9 +1,14 @@
+# imports
+from flask import Flask, render_template, request, redirect, jsonify, make_response
 import csv
+import os, io
 
 def dict_to_csv(dictionary):
-
+    address = request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
+    print(os.path.dirname(__file__) + "\\static\\csv\\" + address)
+    os.makedirs(os.path.dirname(__file__) + "\\static\\csv\\" + address)
     
-    filename = "dictionary_file.csv"
+    filename = os.path.dirname(__file__) + "\\static\\csv\\" + address + "\\dictionary_file.csv"
     with open (filename, "w", newline="") as csvfile:
         fields = ["keyword", "definition"]
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fields)
